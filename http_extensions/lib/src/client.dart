cbcedaa5461071faf1cfd264c2f3dd9c373fc15d
Jogboms <jeremiahogbomo@gmail.com>
2020-05-31 18:29:19 +0100

    Introduce formWithOptions method to ExtendedClient

--- http_extensions/lib/src/client.dart
+++ http_extensions/lib/src/client.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:http_extensions/http_extensions.dart';
+
+import '../helpers.dart';
 import 'request.dart';
 
 class ExtendedClient extends http.BaseClient {
@@ -13,8 +15,7 @@ class ExtendedClient extends http.BaseClient {
     this.extensions = const [],
   }) : root = _buildRoot(inner, extensions);
 
-  static http.BaseClient _buildRoot(
-      http.BaseClient inner, List<Extension> extensions) {
+  static http.BaseClient _buildRoot(http.BaseClient inner, List<Extension> extensions) {
     var result = inner;
     for (var extension in extensions.reversed) {
       extension._inner = result;
@@ -29,36 +30,49 @@ class ExtendedClient extends http.BaseClient {
     return root.send(request);
   }
 
-  Future<http.StreamedResponse> sendWithOptions(
-      http.BaseRequest request, List<dynamic>? options) {
+  Future<http.StreamedResponse> sendWithOptions(http.BaseRequest request, List<dynamic>? options) {
     return root.send(ExtensionRequest(options: options, request: request));
   }
 
-  Future<http.Response> getWithOptions(url,
-          {Map<String, String>? headers, List<dynamic>? options}) =>
+  Future<http.Response> getWithOptions(url, {Map<String, String>? headers, List<dynamic>? options}) =>
       _sendUnstreamedWithOptions('GET', url, headers, options);
 
-  Future<http.Response> headWithOptions(url,
-          {Map<String, String>? headers, List<dynamic>? options}) =>
+  Future<http.Response> headWithOptions(url, {Map<String, String>? headers, List<dynamic>? options}) =>
       _sendUnstreamedWithOptions('HEAD', url, headers, options);
 
-  Future<http.Response> deleteWithOptions(url,
-          {Map<String, String>? headers, List<dynamic>? options}) =>
+  Future<http.Response> deleteWithOptions(url, {Map<String, String>? headers, List<dynamic>? options}) =>
       _sendUnstreamedWithOptions('DELETE', url, headers, options);
 
   Future<http.Response> putWithOptions(url,
-          {Map<String, String>? headers,
-          body,
-          List<dynamic>? options,
-          Encoding? encoding}) =>
+          {Map<String, String>? headers, body, List<dynamic>? options, Encoding? encoding}) =>
       _sendUnstreamedWithOptions('PUT', url, headers, options, body, encoding);
 
   Future<http.Response> postWithOptions(url,
-          {Map<String, String>? headers,
-          body,
-          List<dynamic>? options,
-          Encoding? encoding}) =>
+          {Map<String, String>? headers, body, List<dynamic>? options, Encoding? encoding}) =>
       _sendUnstreamedWithOptions('POST', url, headers, options, body, encoding);
+
+  Future<http.Response> formWithOptions(
+    String url, {
+    Map<String, String>? headers,
+    Map<String, dynamic>? body,
+    List<http.MultipartFile>? files,
+    List<dynamic>? options,
+  }) async {
+    final request = http.MultipartRequest('POST', Uri.parse(url));
+
+    request.headers
+        .addAll({if (headers != null) ...headers, HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded'});
+
+    if (body != null && body.isNotEmpty) {
+      request.fields.addAll(_encodeFormBody(body));
+    }
+
+    if (files != null && files.isNotEmpty) {
+      request.files.addAll(files);
+    }
+
+    return sendWithOptions(request, options).then(http.Response.fromStream);
+  }
 
   /// Sends a non-streaming [Request] and returns a non-streaming [Response].
   Future<http.Response> _sendUnstreamedWithOptions(
@@ -90,8 +104,7 @@ abstract class Extension<TOptions> extends http.BaseClient {
 
   final TOptions defaultOptions;
 
-  Extension({http.BaseClient? inner, required this.defaultOptions})
-      : _inner = inner;
+  Extension({http.BaseClient? inner, required this.defaultOptions}) : _inner = inner;
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
@@ -106,9 +119,18 @@ abstract class Extension<TOptions> extends http.BaseClient {
     return sendWithOptions(request, options);
   }
 
-  Future<http.StreamedResponse> sendWithOptions(
-      http.BaseRequest request, TOptions options) {
+  Future<http.StreamedResponse> sendWithOptions(http.BaseRequest request, TOptions options) {
     assert(_inner != null, 'inner http client must not be null');
     return _inner!.send(request);
   }
+}
+
+String _encodeBody(dynamic value) => json.encode(value);
+
+Map<String, String> _encodeFormBody(Map<String, dynamic> data) {
+  return data.map((key, dynamic value) => MapEntry(key, _isPrimitiveValue(value) ? value : _encodeBody(value)));
+}
+
+bool _isPrimitiveValue(dynamic value) {
+  return [int, double, bool, String].contains(value.runtimeType);
 }
