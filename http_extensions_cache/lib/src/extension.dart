60cdf5b2abae211dc5aa4fd666a8249fe97c97ea
Andrew Coutts <andrewjohncoutts@gmail.com>
2021-02-16 08:45:42 -0500

    Fixes for http_extensions_cache to make map result CachedResponse? (nullable) since map entry might be null

--- http_extensions_cache/lib/src/extension.dart
+++ http_extensions_cache/lib/src/extension.dart
@@ -11,61 +11,64 @@ class CacheExtension extends Extension<CacheOptions> {
   final Logger? logger;
   final CacheStore _globalStore;
 
-  CacheExtension(
-      {CacheOptions defaultOptions = const CacheOptions(), this.logger})
-      : this._globalStore = defaultOptions.store ?? MemoryCacheStore(),
+  CacheExtension({
+    CacheOptions defaultOptions = const CacheOptions(),
+    this.logger,
+  })  : _globalStore = defaultOptions.store ?? MemoryCacheStore(),
         super(defaultOptions: defaultOptions);
 
+  @override
   Future<StreamedResponse> sendWithOptions(
-      BaseRequest request, CacheOptions options) async {
+    BaseRequest request,
+    CacheOptions options,
+  ) async {
     if (options.ignoreCache) {
       return await super.sendWithOptions(request, options);
     }
 
     final cacheId = options.keyBuilder(request);
-    assert(cacheId != null, 'The cache key builder produced an empty key');
     final store = options.store ?? _globalStore;
-    CachedResponse? result = await store.get(cacheId);
+    var cacheResult = await store.get(cacheId);
 
-    var souldUpdate = options.forceUpdate ||
-        result == null ||
-        (!options.forceCache && result.expiry.isBefore(DateTime.now()));
+    var shouldUpdate = options.forceUpdate ||
+        cacheResult == null ||
+        (!options.forceCache && cacheResult.expiry.isBefore(DateTime.now()));
 
-    if (souldUpdate) {
+    if (shouldUpdate) {
       logger?.fine(
           '[$cacheId][${request.url}] Not existing or expired cache, or forced update : starting a new request');
 
       try {
-        if (result != null &&
+        if (cacheResult != null &&
             !request.headers.containsKey(HttpHeaders.ifModifiedSinceHeader)) {
           logger?.fine(
               '[$cacheId][${request.url}] Adding `${HttpHeaders.ifModifiedSinceHeader}` header');
           request.headers[HttpHeaders.ifModifiedSinceHeader] =
-              HttpDate.format(result.downloadedAt);
+              HttpDate.format(cacheResult.downloadedAt);
         }
-        ;
+
         final response = await super.sendWithOptions(request, options);
 
-        if (result != null && response.statusCode == 304) {
+        if (cacheResult != null && response.statusCode == 304) {
           logger?.fine(
               '[$cacheId][${request.url}] Content not modified (status code 304), returning cache');
-          return result;
+          return cacheResult;
         }
 
         final expiry = DateTime.now().add(options.expiry);
-        result = await CachedResponse.fromResponse(response,
+        cacheResult = await CachedResponse.fromResponse(response,
             expiry: expiry, id: cacheId, request: request);
 
-        if (options.shouldBeSaved(result)) {
+        if (options.shouldBeSaved(cacheResult)) {
           logger?.fine(
               '[$cacheId][${request.url}] Saving resulting response to cache ...');
-          await store.set(result);
+          await store.set(cacheResult);
         }
 
-        return result;
+        return cacheResult;
       } catch (e) {
-        if (options.returnCacheOnError && result != null) {
-          return result;
+        if (options.returnCacheOnError && cacheResult != null) {
+          return cacheResult;
         } else {
           rethrow;
         }
@@ -75,14 +78,14 @@ class CacheExtension extends Extension<CacheOptions> {
     logger?.fine(
         '[$cacheId][${request.url}] Not updating, try to use local cache ...');
 
-    if (result == null) {
+    if (cacheResult == null) {
       throw NoCacheAvailableException();
     }
 
     logger?.fine(
         '[$cacheId][${request.url}] Result found in cache for corresponding request');
 
-    return result;
+    return cacheResult;
   }
 }
 
