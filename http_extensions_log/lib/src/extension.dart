76fc2cd89cd7de26db8d410919e0252e45f333c9
Martin Edlman <ac@an.y-co.de>
2021-12-22 16:03:34 +0100

    Request and response logging options can be set separately.
    Signed-off-by: Martin Edlman <ac@an.y-co.de>

--- http_extensions_log/lib/src/extension.dart
+++ http_extensions_log/lib/src/extension.dart
@@ -9,8 +9,7 @@ import 'options.dart';
 class LogExtension extends Extension<LogOptions> {
   final Logger? logger;
 
-  LogExtension({LogOptions defaultOptions = const LogOptions(), this.logger})
-      : super(defaultOptions: defaultOptions);
+  LogExtension({LogOptions defaultOptions = const LogOptions(), this.logger}) : super(defaultOptions: defaultOptions);
 
   void log(String message, LogOptions options) {
     if (logger != null) {
@@ -22,8 +21,7 @@ class LogExtension extends Extension<LogOptions> {
 
   int _id = 0;
 
-  Future<String> _formatRequest(
-      int id, BaseRequest request, LogOptions options) async {
+  Future<String> _formatRequest(int id, BaseRequest request, LogOptions options) async {
     if (request is ExtensionRequest) {
       return _formatRequest(id, request.request, options);
     }
@@ -31,101 +29,91 @@ class LogExtension extends Extension<LogOptions> {
     final requestLog = StringBuffer();
 
     requestLog.writeln('-> REQ($id) [ ${request.method} | ${request.url} ]');
-    if (options.logHeaders) {
-      requestLog
-          .writeln('  * headers: ${request.headers.isEmpty ? 'empty' : ''}');
+    if (options.logRequestHeaders) {
+      requestLog.writeln('  * headers: ${request.headers.isEmpty ? 'empty' : ''}');
       request.headers.forEach((k, v) {
         requestLog.writeln('    * $k: $v');
       });
     }
 
-    if (options.logContent) {
+    if (options.logRequestContent) {
       requestLog.writeln('  * content-length: ${request.contentLength}');
 
-      final bytes = await request.finalize();
+      final bytes = request.finalize();
       final content = await bytes.toBytes();
-      requestLog.writeln(
-          '  * content: ${utf8.decode(content, allowMalformed: true)}');
+      requestLog.writeln('  * content: ${utf8.decode(content, allowMalformed: true)}');
     }
 
     return requestLog.toString();
   }
 
-  Future<String> _formatResponse(
-      int id, StreamedResponse response, LogOptions options) async {
+  Future<String> _formatResponse(int id, StreamedResponse response, LogOptions options) async {
     final requestLog = StringBuffer();
 
-    requestLog.writeln(
-        '<- RES($id) [ ${response.request!.method} | ${response.request!.url}]');
+    requestLog.writeln('<- RES($id) [ ${response.request!.method} | ${response.request!.url}]');
 
     requestLog.writeln('  * status-code: ${response.statusCode}');
 
-    if (options.logHeaders) {
-      requestLog
-          .writeln('  * headers: ${response.headers.isEmpty ? 'empty' : ''}');
+    if (options.logRequestHeaders) {
+      requestLog.writeln('  * headers: ${response.headers.isEmpty ? 'empty' : ''}');
       response.headers.forEach((k, v) {
         requestLog.writeln('    * $k: $v');
       });
     }
 
-    if (options.logContent) {
+    if (options.logResponseContent) {
       requestLog.writeln('  * content-length: ${response.contentLength}');
       final content = await response.stream.toBytes();
-      requestLog.writeln(
-          '  * content: ${utf8.decode(content, allowMalformed: true)}');
+      requestLog.writeln('  * content: ${utf8.decode(content, allowMalformed: true)}');
     }
 
     return requestLog.toString();
   }
 
-  String _formatError(int id, BaseRequest request, dynamic error,
-      StackTrace stackTrace, LogOptions options) {
+  String _formatError(int id, BaseRequest request, dynamic error, StackTrace stackTrace, LogOptions options) {
     final errorLog = StringBuffer();
     errorLog.writeln('!) ERR($id) [ ${request.method} | ${request.url} ]');
 
     if (error != null) {
-      errorLog.writeln('  * error: ${error}');
+      errorLog.writeln('  * error: $error');
     }
 
-    if (stackTrace != null) {
-      errorLog.writeln('  * stack-trace: ${stackTrace}');
-    }
+    errorLog.writeln('  * stack-trace: $stackTrace');
 
     return errorLog.toString();
   }
 
-  Future<StreamedResponse> sendWithOptions(
-      BaseRequest request, LogOptions options) async {
+  @override
+  Future<StreamedResponse> sendWithOptions(BaseRequest request, LogOptions options) async {
     if (!options.isEnabled) {
       return await super.sendWithOptions(request, options);
     }
 
     final id = _id++;
 
-    if (options.logContent) {
+    if (options.logRequestContent) {
       request = BufferedRequest(request);
     }
 
     // Logging request
     final requestLog = await _formatRequest(id, request, options);
-    this.log(requestLog, options);
+    log(requestLog, options);
 
     try {
       var response = await super.sendWithOptions(request, options);
 
-      if (options.logContent) {
+      if (options.logResponseContent) {
         response = BufferedStreamResponse(response);
       }
 
       // Logging response
-      final responsetLog = await _formatResponse(id, response, options);
-      this.log(responsetLog, options);
+      final responseLog = await _formatResponse(id, response, options);
+      log(responseLog, options);
 
       return response;
     } catch (error, stacktrace) {
-      final errorLog =
-          await _formatError(id, request, error, stacktrace, options);
-      this.log(errorLog, options);
+      final errorLog = _formatError(id, request, error, stacktrace, options);
+      log(errorLog, options);
       rethrow;
     }
   }
